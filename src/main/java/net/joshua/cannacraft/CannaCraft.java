e43c53b75ed2a0cdb3e0a41bbc3dc52a18e0e028
dextermovies <dextermovies@yahoo.com>
2022-09-16 15:20:55 -0400

    Fixed Zircon not showing up in game!

--- src/main/java/net/joshua/cannacraft/CannaCraft.java
+++ src/main/java/net/joshua/cannacraft/CannaCraft.java
@@ -4,9 +4,7 @@ import com.mojang.logging.LogUtils;
 import net.joshua.cannacraft.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -37,10 +35,6 @@ public class CannaCraft
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents
     {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
 
-        }
     }
 }
--- src/main/java/net/joshua/cannacraft/item/ModItems.java
+++ src/main/java/net/joshua/cannacraft/item/ModItems.java
@@ -3,22 +3,26 @@ package net.joshua.cannacraft.item;
 import net.joshua.cannacraft.CannaCraft;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final DeferredRegister ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS , CannaCraft.MOD_ID);
 
-    public static final RegistryObject<Item> ZIRCON = ITEMS.register ("zircon",
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            CannaCraft.MOD_ID);
+
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
-    public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register ("raw_zircon",
+    public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
-    public static void register(IEventBus eventbus) {
-        ITEMS.register(eventbus);
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
     }
 }
